5e4fcc1b79913d0fd10245f64a99335d6eab9c3e
titomakelele <titomakelele@gmail.com>
2023-05-09 18:46:36 -0300

    Creacion del nav e inicio de creacion del ruteo

--- router.php
+++ router.php
@@ -3,9 +3,15 @@
     require_once 'templates/estructura.php';
     require_once 'templates/tabla.php';
     require_once 'db_coneccion/peticiones.php';
-
+    $action='';
+    if(!empty($_GET['action'])){
+        $action = explode('/',$_GET['action']);
+    }else{
+        $action = 'tabla';
+    }
 
     mostrarHead();
     mostrarHeader();
+
     mostrarTabla(selecAll());
     mostrarFooter();
--- templates/estructura.php
+++ templates/estructura.php
@@ -15,7 +15,15 @@ function mostrarHead(){
             <body>
     ';
 }
-
+function mostrarNav(){
+    echo "<nav>
+            <ul>
+                <li><a href='#'>materias</a></li>
+                <li><a href='#'>tabla con filtros</a></li>
+                
+            </ul>
+        </nav>";
+} 
 function mostrarHeader(){
     echo'
             <header class="row">
@@ -24,6 +32,9 @@ function mostrarHeader(){
                         Universidad
                     </h1>
                 </div>
+                ';
+                mostrarNav();
+                echo'
             </header>
         <main>
     ';
